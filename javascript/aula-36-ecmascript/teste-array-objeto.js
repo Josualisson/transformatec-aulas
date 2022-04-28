90384ccce5f5ade6949d8a7db31db57d08c7b75b
Josué Alisson <bateriadotransformice@gmail.com>
2022-04-28 17:39:38 -0300

    Brincando

---
+++ javascript/aula-36-ecmascript/teste-array-objeto.js
@@ -0,0 +1,48 @@
+let garagem = [
+    {
+        //0
+        fusca:{
+            cor:"azul",
+            modelo: "fusca",
+            ano: 1965,
+        },
+
+        //1
+        corcel:{
+            cor:"prata",
+            modelo:"corcel",
+            ano:1985
+        },
+
+        //2
+        chevette:{
+            cor: "marrom",
+            modelo:"chevette",
+            ano: 1986
+        }
+    },
+    {
+        //0
+        fusca:{
+            cor:"azul",
+            modelo: "fusca",
+            ano: 1965,
+        },
+
+        //1
+        corcel:{
+            cor:"prata",
+            modelo:"corcel",
+            ano:1985
+        },
+
+        //2
+        chevette:{
+            cor: "marrom",
+            modelo:"chevette",
+            ano: 1986
+        }
+    }
+];
+
+console.log(garagem[0].chevette.cor)
